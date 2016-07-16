897d94129110fd50c45499a1ea60137aca23eeff
Igor Camargo <igor_camargo96@hotmail.com>
2016-07-16 16:52:35 -0300

    resultado->soma

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -14,7 +14,7 @@ public class Calculadora extends JFrame{
     float resultado=0;
     private char operacao='a';
     
-    private static String labelPrefix = "Resultado: ";
+    private static String labelPrefix = "Soma: ";
     final JLabel lblMsg = new JLabel(labelPrefix);
     final JButton btn0 = new JButton("0");
     final JButton btn1 = new JButton("1");
